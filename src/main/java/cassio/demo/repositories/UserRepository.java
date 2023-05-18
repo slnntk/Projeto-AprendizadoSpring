4ff12522c1bdc43ac3edc955c37bf13da4de6cfc
Cássio Tiago <101597036+slnntk@users.noreply.github.com>
2023-05-18 13:45:04 -0300

    service layer, component registration

--- src/main/java/cassio/demo/repositories/UserRepository.java
+++ src/main/java/cassio/demo/repositories/UserRepository.java
@@ -3,7 +3,9 @@ package cassio.demo.repositories;
 import cassio.demo.entities.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository // não precisa dessa anotation, já que ele herda de um Repository.
 public interface UserRepository extends JpaRepository<User, Long>{
 
 
