9c2fd0af95f84b895a77a8d9c9e73f13f99466a9
Cássio Tiago <101597036+slnntk@users.noreply.github.com>
2023-05-18 14:57:15 -0300

    OrderStatus enum

--- src/main/java/cassio/demo/config/TestConfig.java
+++ src/main/java/cassio/demo/config/TestConfig.java
@@ -2,6 +2,7 @@ package cassio.demo.config;
 
 import cassio.demo.entities.User;
 import cassio.demo.entities.Order;
+import cassio.demo.entities.enums.OrderStatus;
 import cassio.demo.repositories.OrderRepository;
 import cassio.demo.repositories.UserRepository;
 
@@ -28,9 +29,9 @@ public class TestConfig implements CommandLineRunner{
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
         //formato iso 8601
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID ,u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT,u1);
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1,o2,o3));
