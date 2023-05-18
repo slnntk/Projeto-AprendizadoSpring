fbb0ed87a2c06951a85ceb79da1fdb0cc5bfaca7
Cássio Tiago <101597036+slnntk@users.noreply.github.com>
2023-05-18 12:58:22 -0300

    H2 database, test profile + JPA

--- src/main/java/cassio/demo/entities/User.java
+++ src/main/java/cassio/demo/entities/User.java
@@ -1,14 +1,20 @@
 package cassio.demo.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serial;
 import java.io.Serializable;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
 
     @Serial
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
