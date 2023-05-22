9767b0ace47b1172f95c0ccb6d5cef58593c8cf1
Cássio Tiago <101597036+slnntk@users.noreply.github.com>
2023-05-22 15:38:15 -0300

    user delete + user insert

--- src/main/java/cassio/demo/resources/UserResource.java
+++ src/main/java/cassio/demo/resources/UserResource.java
@@ -5,11 +5,10 @@ import cassio.demo.entities.User;
 import cassio.demo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -31,4 +30,22 @@ public class UserResource {
         return ResponseEntity.ok().body(u);
     }
 
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj){
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequestUri()
+                .path("/{id}")
+                .buildAndExpand(obj.getId())
+                .toUri();
+        return ResponseEntity.created(uri).body(obj);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity
+                .noContent()
+                .build();
+    }
 }
